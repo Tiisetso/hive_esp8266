1a39be87fe8c7fdeb193f09a8cd217bf379c910b
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2025-07-02 17:26:58 +0300

    updated colours

--- blink.lua
+++ blink.lua
@@ -7,19 +7,66 @@ gpio.mode(red, gpio.OUTPUT)
 gpio.mode(green, gpio.OUTPUT)
 gpio.mode(blue, gpio.OUTPUT)
 
-while true do
-	gpio.write(red, gpio.LOW)
-	gpio.write(green, gpio.HIGH)
-	gpio.write(blue, gpio.HIGH)
-	tmr.delay(255)
+for x=1, 10 do
+	for i=1, 1000 do
+		gpio.write(red, gpio.LOW)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(255)
 
-	gpio.write(red, gpio.HIGH)`
-	gpio.write(green, gpio.LOW)
-	gpio.write(blue, gpio.HIGH)
-	tmr.delay(204)
+		gpio.write(red, gpio.HIGH)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.LOW)
+		tmr.delay(255)
+	end
 
-	gpio.write(red, gpio.HIGH)
-	gpio.write(green, gpio.HIGH)
-	gpio.write(blue, gpio.LOW)
-	tmr.delay(204)
+	for i=1, 1000 do
+		gpio.write(red, gpio.LOW)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(255)
+
+		gpio.write(red, gpio.HIGH)
+		gpio.write(green, gpio.LOW)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(204)
+
+		gpio.write(red, gpio.HIGH)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.LOW)
+		tmr.delay(204)
+	end
+
+	for i=1, 1000 do
+		gpio.write(red, gpio.LOW)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(255)
+
+		gpio.write(red, gpio.HIGH)
+		gpio.write(green, gpio.LOW)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(255)
+	end
+
+	for i=1, 1000 do
+		gpio.write(red, gpio.LOW)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(255)
+
+		gpio.write(red, gpio.HIGH)
+		gpio.write(green, gpio.LOW)
+		gpio.write(blue, gpio.HIGH)
+		tmr.delay(255)
+
+		gpio.write(red, gpio.HIGH)
+		gpio.write(green, gpio.HIGH)
+		gpio.write(blue, gpio.LOW)
+		tmr.delay(255)
+	end
 end
+
+gpio.write(red, gpio.HIGH)
+gpio.write(green, gpio.HIGH)
+gpio.write(blue, gpio.HIGH)
