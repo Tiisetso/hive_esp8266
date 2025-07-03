1555d9fd0e5103c480cffe54ebebd1e16dbf5895
bposa <bruno.posa@gmail.com>
2025-07-03 17:17:42 +0300

    parse with display

--- parse.lua
+++ parse.lua
@@ -39,13 +39,55 @@ function M.arrival_printing(json_text)
 end
 
 ---------------------- TEST with example json ------------------------
-function M.test_arrival_extraction()
-  local json_text = [[
-   {"data":{"stop":{"name":"Haapaniemi","stoptimesWithoutPatterns":[{"realtimeArrival":42870,"headsign":"Rautatientori"},{"realtimeArrival":42936,"headsign":"Rautatientori"},{"realtimeArrival":42947,"headsign":"Rautatientori"},{"realtimeArrival":42969,"headsign":"Rautatientori"},{"realtimeArrival":42974,"headsign":"Rautatientori"}]}}}
-  ]]
+-- function M.test_arrival_extraction()
+--   local json_text = [[
+--    {"data":{"stop":{"name":"Haapaniemi","stoptimesWithoutPatterns":[{"realtimeArrival":42870,"headsign":"Rautatientori"},{"realtimeArrival":42936,"headsign":"Rautatientori"},{"realtimeArrival":42947,"headsign":"Rautatientori"},{"realtimeArrival":42969,"headsign":"Rautatientori"},{"realtimeArrival":42974,"headsign":"Rautatientori"}]}}}
+--   ]]
   
-  M.arrival_printing(json_text)
+--   M.arrival_printing(json_text)
+-- end
+
+
+-- M.test_arrival_extraction()
+
+
+id  = 0
+sda = 2
+scl = 1
+sla = 0x3C
+
+
+local u8g2 = require("u8g2")
+-- local parse = require("parse")
+i2c.setup(id, sda, scl, i2c.SLOW)
+local disp = u8g2.ssd1306_i2c_128x64_noname(id, sla)
+
+
+local json = [[
+  {"data":{"stop":{"name":"Haapaniemi","stoptimesWithoutPatterns":[
+    {"realtimeArrival":4,"headsign":"Rautatientori"},
+    {"realtimeArrival":7,"headsign":"Kamppi"},
+    {"realtimeArrival":15,"headsign":"Otaniemi"}
+  ]}}}
+]]
+
+
+local arrivals = M.get_num_values(json, "realtimeArrival")
+
+
+disp:clearBuffer()
+disp:setFont(u8g2.font_9x15_tf)
+disp:drawStr(0, 16, "Next Buses:")
+
+if #arrivals == 0 then
+  disp:drawStr(0, 36, "No arrivals.")
+else
+  local y = 36
+  for i = 1, math.min(3, #arrivals) do
+    local msg = "In: " .. arrivals[i] .. " sec"
+    disp:drawStr(0, y, msg)
+    y = y + 16
+  end
 end
 
-
-M.test_arrival_extraction()
+disp:sendBuffer()
