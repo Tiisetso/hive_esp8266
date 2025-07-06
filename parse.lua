cba6ead935946ae2c78d770e1bacef00cd4b7824
bposa <bruno.posa@gmail.com>
2025-07-06 17:27:05 +0300

    parsing time to arrival

--- parse.lua
+++ parse.lua
@@ -3,58 +3,46 @@ local P = {}
 
 --- Returns array of digit values after each search_term  in a given string
 function P.get_num_values(json_text, search_term)
-  local arrivals = {}
+  local value = {}
   local pattern = '"' .. search_term .. '"%s*:%s*"?(%d+)"?'
   for num in json_text:gmatch(pattern) do
-    table.insert(arrivals, num)
+    table.insert(value, num)
   end
-  return arrivals
+  return value
 end
 
--- prints the results with up to max arrivals, or 'no arrivals' if none
--- prefers realtime to scheduled
-function P.arrival_printing(json_text)
-  local max = 3
-  
-  local arrivals = P.get_num_values(json_text, "realtimeArrival")
-  --we want the realtime ideally but it may not be there
-  if #arrivals == 0 then
-    arrivals = P.get_num_values(json_text, "scheduledArrival")
-    if #arrivals == 0 then
-      print("No arrivals.")
-    end
+
+function P.time(sec_from_midnight)
+  local now = rtctime.get()
+  if not now then
+    return "?"  -- RTC not synced
+  end
+
+  local cal = rtctime.epoch2cal(now)
+  local current_sec = cal.hour * 3600 + cal.min * 60 + cal.sec
+  local delta = sec_from_midnight - current_sec
+
+  if delta < 0 then
+    return "0"  -- Bus has already arrived
   else
-    for i = 1, math.min(#arrivals, max) do
-      if i == 1 then
-        print("First arrival:", arrivals[1])
-      elseif i == 2 then
-        print("next arrivals:")
-        print(" " .. arrivals[i])
-      else
-        print(" " .. arrivals[i])
-      end
-    end
+    return tostring(math.floor(delta / 60))
   end
-
 end
-
 
 
 function P.arrival_display(json)
-  id  = 0
-  sda = 2
-  scl = 1
-  sla = 0x3C
+  local id  = 0
+  local sda = 2
+  local scl = 1
+  local sla = 0x3C
   local u8g2 = require("u8g2")
-  -- local parse = require("parse")
   i2c.setup(id, sda, scl, i2c.SLOW)
   local disp = u8g2.ssd1306_i2c_128x64_noname(id, sla)
 
-  -- local json = require("data")
-  -- json = data
-
+  local now = rtctime.get()
+  print("Time rtc: ", now)--debug
   local arrivals = P.get_num_values(json, "realtimeArrival")
-
+  local busses = P.get_num_values(json, "shortName")
 
   disp:clearBuffer()
   disp:setFont(u8g2.font_6x10_tf)
@@ -65,7 +53,7 @@ function P.arrival_display(json)
   else
     local y = 36
     for i = 1, math.min(3, #arrivals) do
-      local msg = "In: " .. arrivals[i] .. " sec"
+      local msg = busses[i] .. " in " .. P.time(arrivals[i]) .. " minutes"
       disp:drawStr(0, y, msg)
       y = y + 10
     end
